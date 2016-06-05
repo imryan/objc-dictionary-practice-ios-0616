a9435941ec69e4ebffb93faa9f6b648f3870483f
imryan <ryan.cohen@pace.edu>
2016-06-05 17:40:48 -0400

    done

--- DictionaryPracticeInObjectiveC/AppDelegate.m
+++ DictionaryPracticeInObjectiveC/AppDelegate.m
@@ -17,15 +17,59 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSDictionary *favoriteBands = @{
+                                    @"Nirvana"             : @"",
+                                    @"Stone Temple Pilots" : @"",
+                                    @"Smashing Pumpkins"   : @"",
+                                    };
     
-    /**
-     
-     *  Write your code here!
-     
-     */
+    NSLog(@"My favorite bands are: %@", favoriteBands);
     
-    // Do not alter
-    return YES;  //
-}   ///////////////
+    NSMutableDictionary *mFavoriteBands = [NSMutableDictionary dictionaryWithDictionary:favoriteBands];
+    mFavoriteBands[@"Nirvana"] = @"MTV Unplugged";
+    mFavoriteBands[@"Stone Temple Pilots"] = @"Thank You";
+    mFavoriteBands[@"Smashing Pumpkins"] = @"Siamese Dream";
+    
+    NSLog(@"My favorite bands are: %@", mFavoriteBands);
+    
+    [mFavoriteBands removeObjectForKey:@"Smashing Pumpkins"];
+    
+    NSLog(@"My favorite bands are: %@", mFavoriteBands);
+    
+    NSArray *favoriteBandKeys = [mFavoriteBands allKeys];
+    
+    for (NSString *band in favoriteBandKeys) {
+        NSLog(@"%@'s album: %@", band, [mFavoriteBands objectForKey:band]);
+    }
+    
+    NSDictionary *thankYouAlbum = @{
+                                     @1 : @"Creep",
+                                     @2 : @"Plush",
+                                     @3 : @"Big Empty",
+                                   };
+    
+    NSDictionary *unpluggedAlbum = @{
+                                    @1 : @"About A Girl",
+                                    @2 : @"Come As You Are",
+                                    @3 : @"Oh Me",
+                                    };
+    
+    
+    [mFavoriteBands setObject:thankYouAlbum forKey:@"Stone Temple Pilots"];
+    [mFavoriteBands setObject:unpluggedAlbum forKey:@"Nirvana"];
+    
+    for (NSString *track in [mFavoriteBands[@"Stone Temple Pilots"] allKeys]) {
+        NSLog(@"Song: %@", [mFavoriteBands[@"Stone Temple Pilots"] objectForKey:track]);
+    }
+    
+    NSMutableDictionary *favoriteAlbums = [NSMutableDictionary new];
+    for (NSDictionary *band in [mFavoriteBands allKeys]) {
+        [favoriteAlbums addEntriesFromDictionary:[mFavoriteBands objectForKey:band]];
+    }
+    
+    NSLog(@"Favorite albums: %@", favoriteAlbums);
+    
+    return YES;
+}
 
 @end
